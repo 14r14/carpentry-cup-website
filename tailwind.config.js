af134f484a51487f0af8318d1922f0922d150989
Rohan Ramakrishnan <65002749+14r14@users.noreply.github.com>
2023-01-17 18:39:57 +0000

    add dm sans and dela gothic one

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dela-gothic': ['Dela Gothic One', 'cursive'],
+        'dm-sans': ['DM Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
